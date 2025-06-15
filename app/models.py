52fc08799e9306e4dae5f0cacfc784b2f391ea0e
Kevin Ngo <kevinngo2002@gmail.com>
2025-06-14 23:53:03 -0400

    Profile Page and Avatars checkpoint

--- app/models.py
+++ app/models.py
@@ -1,35 +1,55 @@
-from typing import Optional
 from datetime import datetime, timezone
+from hashlib import md5
+from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import db
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import db, login
 
-#goal: make a table named users with an id column that is an integer and is the primary key
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
-    username: so.Mapped[str] = so.mapped_column(sa.String(64), unique=True, nullable=False)
-    email: so.Mapped[Optional[str]] = so.mapped_column(sa.String(120), unique=True, nullable=True)
-    password_hash: so.Mapped[str] = so.mapped_column(sa.String(256), nullable=False)
+    username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
+                                                unique=True)
+    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True,
+                                             unique=True)
+    password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+    about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
+    last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(
+        default=lambda: datetime.now(timezone.utc))
 
-    posts: so.WriteOnlyMapped["Post"] = so.relationship(back_populates="author", cascade="all, delete-orphan")
+    posts: so.WriteOnlyMapped['Post'] = so.relationship(
+        back_populates='author')
 
     def __repr__(self):
-        return f'<User {self.username}>'
-    
+        return '<User {}>'.format(self.username)
 
-#id integer, primary key
-#body varchar(140)
-#timestamp datetime
-#user_id integer
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
+
+
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
+
 
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     body: so.Mapped[str] = so.mapped_column(sa.String(140))
-    timestamp: so.Mapped[datetime] = so.mapped_column(sa.DateTime, index=True, default=lambda: datetime.now(timezone.utc))
-    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey('user.id'))
+    timestamp: so.Mapped[datetime] = so.mapped_column(
+        index=True, default=lambda: datetime.now(timezone.utc))
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id),
+                                               index=True)
 
-    author: so.Mapped["User"] = so.relationship('User', back_populates='posts')
+    author: so.Mapped[User] = so.relationship(back_populates='posts')
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
