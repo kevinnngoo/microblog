f9a2f50d601851e3b8a0616589b1eab45ad043e7
Kevin Ngo <kevinngo2002@gmail.com>
2025-06-15 23:40:22 -0400

    Followers and unit tests

--- app/forms.py
+++ app/forms.py
@@ -44,10 +44,14 @@ class EditProfileForm(FlaskForm):
     def __init__(self, original_username, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.original_username = original_username
-    
+
     def validate_username(self, username):
         if username.data != self.original_username:
             user = db.session.scalar(sa.select(User).where(
                 User.username == username.data))
             if user is not None:
                 raise ValidationError('Please use a different username.')
+
+
+class EmptyForm(FlaskForm):
+    submit = SubmitField('Submit')
